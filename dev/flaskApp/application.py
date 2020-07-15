2633f6f0283a22fe2072b03a895f448a9bb37df1
Amisha Patel <amishap79@gmail.com>
2020-07-15 14:56:54 -0400

    added FL/NY/TX pages & modified CA

--- dev/flaskApp/application.py
+++ dev/flaskApp/application.py
@@ -10,6 +10,18 @@ def hello():
 def california():
     return render_template('california.html')
 
+@application.route('/florida')
+def florida():
+    return render_template('florida.html')
+
+@application.route('/newyork')
+def newyork():
+    return render_template('newyork.html')
+
+@application.route('/texas')
+def texas():
+    return render_template('texas.html')
+
 
 if __name__ == '__main__':
     application.run(debug=True)
